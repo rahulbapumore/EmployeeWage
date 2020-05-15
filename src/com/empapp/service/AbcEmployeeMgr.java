68eeb9c319c5428518725505ae398c5cb11804db
Rahul <rahulbapumore@gmail.com>
2020-05-15 23:17:39 +0530

    Calculating daily employee wage

---
+++ src/com/empapp/service/AbcEmployeeMgr.java
@@ -0,0 +1,42 @@
+package com.empapp.controller;
+import com.empapp.model.Employee;
+
+public class AbcEmployeeMgr implements IEmployeeManager
+{
+   public static final int workingHours = 8;
+   public static final int hourlyWage = 30;
+
+
+  @Override
+  public boolean isEmployeePresent()
+  {
+        if(Math.round(Math.random()) == 0)
+         return false ;
+        else
+         return true;
+
+  }
+   
+  @Override
+  public int calculateDailyWage(Employee emp)
+  {
+        if(emp.getIsPresent() == true)
+        {
+            if(emp.getDoneParttime() == true)
+            {
+                return (hourlyWage*workingHours + hourlyWage*workingHours );
+            }
+            else
+            {
+                return (hourlyWage*workingHours );
+            }
+        }
+	return 0;
+  }
+  
+
+
+       
+}
+
+
